9d385b21816bf84f5f94d9c56d4d1a44cdea9add
Daniel Shaulov <danielshaulov1@gmail.com>
2026-07-26 13:20:32 +0300

    multi-region-flow-logs

--- flow_logs.tf
+++ flow_logs.tf
@@ -12,6 +12,37 @@ locals {
       ]
     ]) : item.key => item
   } : {}
+
+  # Unique subscription+region combinations that have VNets
+  vnet_storage_account_keys = var.enable_vnet_flow_logs ? toset([
+    for item in flatten([
+      for sub_id in var.subscription_ids : [
+        for vnet in try(data.azapi_resource_list.vnets[sub_id].output.value, []) : {
+          key             = format("%s|%s", sub_id, vnet.location)
+          subscription_id = sub_id
+          location        = vnet.location
+        }
+      ]
+    ]) : item
+  ]) : toset([])
+
+  vnet_storage_accounts = {
+    for item in local.vnet_storage_account_keys :
+    item.key => item
+  }
+
+  vnet_storage_account_names = {
+    for key, item in local.vnet_storage_accounts :
+    key => substr(
+      format(
+        "skhflow%s%s",
+        substr(replace(item.subscription_id, "-", ""), 0, 10),
+        substr(replace(item.location, "-", ""), 0, 6),
+      ),
+      0,
+      24,
+    )
+  }
 }
 
 # List all VNets per subscription
@@ -34,6 +65,51 @@ resource "azapi_resource_action" "register_network_provider" {
   method      = "POST"
 }
 
+# Resource group per subscription+region for flow log storage accounts
+resource "azapi_resource" "vnet_flow_log_resource_group" {
+  for_each = local.vnet_storage_accounts
+
+  type      = "Microsoft.Resources/resourceGroups@2021-04-01"
+  name      = substr(lower(format("skh-flowlogs-%s-rg", replace(each.value.location, " ", "-"))), 0, 90)
+  parent_id = format("/subscriptions/%s", each.value.subscription_id)
+
+  body = {
+    location = each.value.location
+  }
+
+  depends_on = [azapi_resource_action.register_network_provider]
+}
+
+# One storage account per subscription+region to match flow log location requirement
+resource "azapi_resource" "vnet_flow_log_storage_account" {
+  for_each = local.vnet_storage_accounts
+
+  type      = "Microsoft.Storage/storageAccounts@2023-01-01"
+  name      = local.vnet_storage_account_names[each.key]
+  parent_id = azapi_resource.vnet_flow_log_resource_group[each.key].id
+
+  body = {
+    location = each.value.location
+    sku = {
+      name = "Standard_LRS"
+    }
+    kind = "StorageV2"
+    properties = {
+      allowBlobPublicAccess    = false
+      minimumTlsVersion        = "TLS1_2"
+      supportsHttpsTrafficOnly = true
+      networkAcls = {
+        bypass              = "AzureServices"
+        defaultAction       = "Allow"
+        ipRules             = []
+        virtualNetworkRules = []
+      }
+    }
+  }
+
+  depends_on = [azapi_resource.vnet_flow_log_resource_group]
+}
+
 # Create VNet Flow Log for each discovered VNet
 resource "azapi_resource" "vnet_flow_log" {
   for_each = local.discovered_vnets
@@ -50,7 +126,7 @@ resource "azapi_resource" "vnet_flow_log" {
   body = {
     properties = {
       enabled          = true
-      storageId        = azapi_resource.storage_account[each.value.subscription_id].id
+      storageId        = azapi_resource.vnet_flow_log_storage_account[format("%s|%s", each.value.subscription_id, each.value.location)].id
       targetResourceId = each.value.vnet_id
       format = {
         type    = "JSON"
@@ -65,6 +141,6 @@ resource "azapi_resource" "vnet_flow_log" {
 
   depends_on = [
     azapi_resource_action.register_network_provider,
-    azapi_resource.storage_account,
+    azapi_resource.vnet_flow_log_storage_account,
   ]
 }
